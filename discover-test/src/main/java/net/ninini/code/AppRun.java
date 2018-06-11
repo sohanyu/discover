5c7acfe698c7233216df37a543e83ae5510dd85f
sohanyu@126.com <sohanyu@126.com>
2018-06-12 03:55:40 +0800

    项目初建

---
+++ discover-test/src/main/java/net/ninini/code/AppRun.java
@@ -0,0 +1,15 @@
+package net.ninini.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+}
---
+++ discover-test/src/main/java/net/ninini/code/controller/TestController.java
@@ -0,0 +1,15 @@
+package net.ninini.code.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello " + name + "，this is two messge";
+    }
+}
