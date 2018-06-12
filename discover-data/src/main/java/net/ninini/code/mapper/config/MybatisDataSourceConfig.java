cea4423d7b4f90e13f0d079c5289f34cc39b173d
sohanyu@126.com <sohanyu@126.com>
2018-06-13 04:42:27 +0800

    mybatis集成

---
+++ discover-data/src/main/java/net/ninini/code/mapper/config/MybatisDataSourceConfig.java
@@ -0,0 +1,224 @@
+package net.ninini.code.mapper.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.io.VFS;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Configuration
+@MapperScan(basePackages = "net.ninini.code.mapper", sqlSessionTemplateRef = "mybatisMasterSqlSessionTemplate")
+@EnableTransactionManagement
+public class MybatisDataSourceConfig implements TransactionManagementConfigurer {
+
+    private Logger logger = LoggerFactory.getLogger(MybatisDataSourceConfig.class);
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    //@Value("{spring.datasource.connectionProperties}")
+    //private String connectionProperties;
+
+    @Bean(name = "dataSource", destroyMethod = "close")
+    @Primary
+    public DataSource dataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(this.dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+
+        //configuration
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        datasource.setPoolPreparedStatements(poolPreparedStatements);
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        try {
+            datasource.setFilters(filters);
+            datasource.init();
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+        //datasource.setConnectionProperties(connectionProperties);
+
+        return datasource;
+    }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        reg.addInitParameter("loginUsername", "druid");
+        reg.addInitParameter("loginPassword", "jiajian123456");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        return filterRegistrationBean;
+    }
+
+
+    @Resource(name = "dataSource")
+    DataSource dataSource;
+
+    @Bean(name = "transactionManager")
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "mybatisMasterSqlSessionFactory")
+    //public SqlSessionFactory mybatisMasterSqlSessionFactory(@Qualifier("mybatisMasterDataSource") DataSource dataSource) throws Exception {
+    public SqlSessionFactory mybatisMasterSqlSessionFactory() throws Exception {
+
+        VFS.addImplClass(SpringBootVFS.class);
+
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*/*.xml"));
+        bean.setVfs(SpringBootVFS.class);
+        // 设置MyBatis分页插件
+        PageInterceptor pageInterceptor = this.initPageInterceptor();
+        bean.setPlugins(new Interceptor[]{pageInterceptor});
+        //bean.setTypeAliasesPackage(InitCons.MYBATIS_ALIASES_PACKAGE);
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        bean.setConfiguration(configuration);
+
+
+        return bean.getObject();
+    }
+
+    @Bean(name = "mybatisMasterTransactionManager")
+    public DataSourceTransactionManager mybatisMasterTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "mybatisMasterSqlSessionTemplate")
+    public SqlSessionTemplate mybatisMasterSqlSessionTemplate(@Qualifier("mybatisMasterSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mScannerConfigurer = new MapperScannerConfigurer();
+        mScannerConfigurer.setSqlSessionFactoryBeanName("mybatisMasterSqlSessionFactory");
+        mScannerConfigurer.setBasePackage("net.ninini.code.entity");
+        //mScannerConfigurer.setBasePackage("net.ninini.code.mapper");
+        return mScannerConfigurer;
+    }
+
+
+    public PageInterceptor initPageInterceptor() {
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "mysql");
+        properties.setProperty("offsetAsPageNum", "true");
+        properties.setProperty("rowBoundsWithCount", "true");
+        pageInterceptor.setProperties(properties);
+        return pageInterceptor;
+    }
+
+
+    //@Bean(name = "mybatisMasterDataSource")
+    //@ConfigurationProperties(prefix = "datasource")
+    //public DataSource mybatisMasterDataSource() {
+    //return new DriverManagerDataSource("jdbc:mysql://localhost:3306/test","root","root");
+    //return DataSourceBuilder.create().build();
+    //}
+
+
+}
---
+++ discover-data/src/main/java/net/ninini/code/mapper/config/MybatisDataSourceConfigBak.java
@@ -0,0 +1,76 @@
+//package net.ninini.code.mapper.config;
+//
+//import com.github.pagehelper.PageInterceptor;
+//import org.apache.ibatis.plugin.Interceptor;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.mybatis.spring.SqlSessionFactoryBean;
+//import org.mybatis.spring.SqlSessionTemplate;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.mapper.MapperScannerConfigurer;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+//
+//import javax.annotation.Resource;
+//import javax.sql.DataSource;
+//import java.util.Properties;
+//
+//@Configuration
+//@MapperScan(basePackages = "net.ninini.code.mapper", sqlSessionTemplateRef = "mybatisMasterSqlSessionTemplate")
+//@EnableTransactionManagement
+//public class MybatisDataSourceConfigBak implements TransactionManagementConfigurer{
+//
+//
+//
+//    @Bean(name = "transactionManager")
+//    @Override
+//    public PlatformTransactionManager annotationDrivenTransactionManager() {
+//        return new DataSourceTransactionManager(dataSource);
+//    }
+//
+//    @Bean
+//    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+//        return new DataSourceTransactionManager(dataSource);
+//    }
+//
+//    @Bean(name = "mybatisMasterDataSource")
+//    @ConfigurationProperties(prefix = "datasource")
+//    public DataSource mybatisMasterDataSource() {
+////        return new DriverManagerDataSource("jdbc:mysql://localhost:3306/test","root","root");
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean(name = "mybatisMasterSqlSessionFactory")
+//    public SqlSessionFactory mybatisMasterSqlSessionFactory(@Qualifier("mybatisMasterDataSource") DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setDataSource(dataSource);
+//        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:com.mybatis.mapper/*.xml"));
+//
+//        // 设置MyBatis分页插件
+//        PageInterceptor pageInterceptor = this.initPageInterceptor();
+//        bean.setPlugins(new Interceptor[]{pageInterceptor});
+//
+//        return bean.getObject();
+//    }
+//
+//    @Bean
+//    public MapperScannerConfigurer mapperScannerConfigurer() {
+//        MapperScannerConfigurer mScannerConfigurer = new MapperScannerConfigurer();
+//        mScannerConfigurer.setSqlSessionFactoryBeanName("mybatisMasterSqlSessionFactory");
+//        mScannerConfigurer.setBasePackage("com.my.boot.test.entity");
+//        mScannerConfigurer.setBasePackage("com.my.boot.test.mapper");
+//        return mScannerConfigurer;
+//    }
+//
+//
+//
+//
+//
+//}
