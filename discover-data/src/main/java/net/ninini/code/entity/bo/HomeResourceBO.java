5a3b9a0d56ee295baa604ed2eb5d12ca9ce4a5de
sohanyu@126.com <sohanyu@126.com>
2018-09-13 10:11:48 +0800

    房源表字段修改

--- discover-data/src/main/java/net/ninini/code/entity/bo/HomeResourceBO.java
+++ discover-data/src/main/java/net/ninini/code/entity/bo/HomeResourceBO.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class HomeResourceBO implements Serializable {
+
     private Long pid;
 
     private Integer area;
@@ -67,7 +68,55 @@ public class HomeResourceBO implements Serializable {
 
     private Integer sort;
 
+    private Integer direction;
+
+    private Integer loft;
+
+    private Integer fitment;
+
+    private Date birthday;
+
+    private Integer trait;
+
+    private String seeTime;
+
+    private Integer optimization;
+
+    private BigDecimal realSpace;
+
+    private Integer buildStructure;
+
+    private String stairsRatio;
+
+    private Integer heating;
+
+    private Integer elevator;
+
+    private Date hangoutTime;
+
+    private Integer ownership;
+
+    private Date lastUpdateTime;
+
+    private Integer boughtTime;
+
+    private Integer propertyRightAttribute;
+
+    private String pledge;
+
+    private BigDecimal lat;
+
+    private BigDecimal lng;
+
+    private Integer buildingNumber;
+
+    private Integer houseUnit;
+
+    private Long agentPid;
+
     private String description;
+
+    private String permit;
 
 
     public HomeResourceBO() {
@@ -105,6 +154,30 @@ public class HomeResourceBO implements Serializable {
         this.del = tHomeResource.getDel();
         this.sort = tHomeResource.getSort();
         this.description = tHomeResource.getDescription();
+        this.direction = tHomeResource.getDirection();
+        this.loft = tHomeResource.getLoft();
+        this.fitment = tHomeResource.getFitment();
+        this.birthday = tHomeResource.getBirthday();
+        this.trait = tHomeResource.getTrait();
+        this.seeTime = tHomeResource.getSeeTime();
+        this.optimization = tHomeResource.getOptimization();
+        this.realSpace = tHomeResource.getRealSpace();
+        this.buildStructure = tHomeResource.getBuildStructure();
+        this.stairsRatio = tHomeResource.getStairsRatio();
+        this.heating = tHomeResource.getHeating();
+        this.elevator = tHomeResource.getElevator();
+        this.hangoutTime = tHomeResource.getHangoutTime();
+        this.ownership = tHomeResource.getOwnership();
+        this.lastUpdateTime = tHomeResource.getLastUpdateTime();
+        this.boughtTime = tHomeResource.getBoughtTime();
+        this.propertyRightAttribute = tHomeResource.getPropertyRightAttribute();
+        this.pledge = tHomeResource.getPledge();
+        this.lat = tHomeResource.getLat();
+        this.lng = tHomeResource.getLng();
+        this.buildingNumber = tHomeResource.getBuildingNumber();
+        this.houseUnit = tHomeResource.getHouseUnit();
+        this.agentPid = tHomeResource.getAgentPid();
+        this.permit = tHomeResource.getPermit();
     }
 
 
@@ -348,11 +421,203 @@ public class HomeResourceBO implements Serializable {
         this.sort = sort;
     }
 
+    public Integer getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Integer direction) {
+        this.direction = direction;
+    }
+
+    public Integer getLoft() {
+        return loft;
+    }
+
+    public void setLoft(Integer loft) {
+        this.loft = loft;
+    }
+
+    public Integer getFitment() {
+        return fitment;
+    }
+
+    public void setFitment(Integer fitment) {
+        this.fitment = fitment;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getTrait() {
+        return trait;
+    }
+
+    public void setTrait(Integer trait) {
+        this.trait = trait;
+    }
+
+    public String getSeeTime() {
+        return seeTime;
+    }
+
+    public void setSeeTime(String seeTime) {
+        this.seeTime = seeTime;
+    }
+
+    public Integer getOptimization() {
+        return optimization;
+    }
+
+    public void setOptimization(Integer optimization) {
+        this.optimization = optimization;
+    }
+
+    public BigDecimal getRealSpace() {
+        return realSpace;
+    }
+
+    public void setRealSpace(BigDecimal realSpace) {
+        this.realSpace = realSpace;
+    }
+
+    public Integer getBuildStructure() {
+        return buildStructure;
+    }
+
+    public void setBuildStructure(Integer buildStructure) {
+        this.buildStructure = buildStructure;
+    }
+
+    public String getStairsRatio() {
+        return stairsRatio;
+    }
+
+    public void setStairsRatio(String stairsRatio) {
+        this.stairsRatio = stairsRatio;
+    }
+
+    public Integer getHeating() {
+        return heating;
+    }
+
+    public void setHeating(Integer heating) {
+        this.heating = heating;
+    }
+
+    public Integer getElevator() {
+        return elevator;
+    }
+
+    public void setElevator(Integer elevator) {
+        this.elevator = elevator;
+    }
+
+    public Date getHangoutTime() {
+        return hangoutTime;
+    }
+
+    public void setHangoutTime(Date hangoutTime) {
+        this.hangoutTime = hangoutTime;
+    }
+
+    public Integer getOwnership() {
+        return ownership;
+    }
+
+    public void setOwnership(Integer ownership) {
+        this.ownership = ownership;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Integer getBoughtTime() {
+        return boughtTime;
+    }
+
+    public void setBoughtTime(Integer boughtTime) {
+        this.boughtTime = boughtTime;
+    }
+
+    public Integer getPropertyRightAttribute() {
+        return propertyRightAttribute;
+    }
+
+    public void setPropertyRightAttribute(Integer propertyRightAttribute) {
+        this.propertyRightAttribute = propertyRightAttribute;
+    }
+
+    public String getPledge() {
+        return pledge;
+    }
+
+    public void setPledge(String pledge) {
+        this.pledge = pledge;
+    }
+
+    public BigDecimal getLat() {
+        return lat;
+    }
+
+    public void setLat(BigDecimal lat) {
+        this.lat = lat;
+    }
+
+    public BigDecimal getLng() {
+        return lng;
+    }
+
+    public void setLng(BigDecimal lng) {
+        this.lng = lng;
+    }
+
+    public Integer getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(Integer buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public Integer getHouseUnit() {
+        return houseUnit;
+    }
+
+    public void setHouseUnit(Integer houseUnit) {
+        this.houseUnit = houseUnit;
+    }
+
+    public Long getAgentPid() {
+        return agentPid;
+    }
+
+    public void setAgentPid(Long agentPid) {
+        this.agentPid = agentPid;
+    }
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getPermit() {
+        return permit;
+    }
+
+    public void setPermit(String permit) {
+        this.permit = permit;
     }
 }
